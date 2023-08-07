42c5485cea8ae9f533f9dfac53b51c0ed3be651a
siudn <cshadx@gmail.com>
2023-08-07 05:18:14 -0400

    Completed pynative_15

---
+++ pynative15/pynative_15.py
@@ -0,0 +1,9 @@
+# Write a function called exponent(base, exp) that returns
+# an int value of base raises to the power of exp.
+# Note here exp is a non-negative integer, and the base is an integer.
+
+def exponent(base, exp):
+    return base ** exp
+
+print(exponent(2,5))
+print(exponent(5,4))
